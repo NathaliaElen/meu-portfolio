09d4213cd44ef4f5b66bd6d16db4c1b8429f236c
Nathália Elen <nathaliaelen79@gmail.com>
2022-10-03 09:16:32 -0300

    Incluindo a função scroll e var typing no Javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
+
+var typed = new Typed(".typing",
+{
+    strings:["Developer Front-End"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2",
+{
+    strings:["Developer Front-End"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
